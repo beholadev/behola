1e831c486634580b66450b0ab6a02be89f01cac5
beholadev <beholadev@gmail.com>
2024-01-03 19:06:15 -0800

    Test From Laptop

--- app/(Step-D-LandingPage)/(routes)/page.tsx
+++ app/(Step-D-LandingPage)/(routes)/page.tsx
@@ -4,9 +4,9 @@ import { Button } from "@/components/ui/button";
 const home = () => {
   return (
     <div className="flex items-center justify-between">
-      <Button variant="outline"> Test Button </Button>
+      <Button variant="outline"> Test Button 10 </Button>
       <div className="pt-5">LANDING PAGE</div> 
-      <Button variant="destructive">Test Button 2 </Button>
+      <Button variant="destructive">Test Button 20 </Button>
     </div>
   );
 }
